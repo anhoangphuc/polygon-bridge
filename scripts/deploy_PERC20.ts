aeb172eeca1817dea548e1d4934c5fa66d0439a2
hoangphuc <hoangphucnb97@gmail.com>
2022-06-14 15:05:36 +0700

    Add script to deploy PERC20

---
+++ scripts/deploy_PERC20.ts
@@ -0,0 +1,19 @@
+import { ethers, network, run } from "hardhat";
+import { saveContract } from "./util";
+
+(async function main() {
+    const networkName = network.name;
+    console.log(`Start deployling contract`);
+    const PERC20 = await ethers.getContractFactory("PERC20");
+    const perc20 = await PERC20.deploy();
+    await perc20.deployed();
+
+    console.log(`Deploy PERC20 token at address ${perc20.address}`);
+    await saveContract(networkName, 'PERC20', perc20.address);
+
+    console.log(`Start verifying contract`);
+    await run('verify:verify', {
+        address: perc20.address,
+    });
+    console.log(`Verifing success`);
+})();
