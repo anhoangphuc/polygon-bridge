2117e2be2e34c5d88395cba857fa48cb53a93264
hoangphuc <hoangphucnb97@gmail.com>
2022-06-15 17:04:18 +0700

    Add method withdrawStart

--- scripts/pos_bridge/move_derc20_to_polygon.ts
+++ scripts/pos_bridge/move_derc20_to_polygon.ts
@@ -1,12 +1,13 @@
 import { POSClient } from "@maticnetwork/maticjs";
+import { strictEqual } from "assert";
 import { BigNumber } from "ethers";
 import { address, getContracts, getPosClient } from "../util";
 
-const networkName = 'goerli';
-const derc20Address = getContracts()[networkName]['DERC20'];
+const derc20AddressParent = getContracts()['goerli']['DERC20'];
+const derc20AddressChild = getContracts()['polygon']['DERC20'];
 
 async function approve(posClient: POSClient, amount: string) {
-    const derc20Token = posClient.erc20(derc20Address, true);
+    const derc20Token = posClient.erc20(derc20AddressParent, true);
     console.log(`Before allowance`);
     const allowance1 = await derc20Token.getAllowance(address);
     console.log(`Allowance1 is ${allowance1}`);
@@ -23,15 +24,24 @@ async function approve(posClient: POSClient, amount: string) {
 }
 
 async function deposit(posClient: POSClient, amount: string) {
-    const derc20Token = posClient.erc20(derc20Address, true);
+    const derc20Token = posClient.erc20(derc20AddressParent, true);
     console.log(`Old allowance is ${await derc20Token.getAllowance(address)}`);
     const depositTx = await derc20Token.deposit(amount, address);
     console.log(`Txhash of depositTx is ${await depositTx.getTransactionHash()} and receipt is ${await depositTx.getReceipt()}`);
     console.log(`New allowance is ${await derc20Token.getAllowance(address)}`);
 }
 
+async function withdraw(posClient: POSClient, amount: string) {
+    const derc20Token = posClient.erc20(derc20AddressChild);
+    console.log(`Old balance is ${await derc20Token.getBalance(address)}`);
+    const withdrawTx = await derc20Token.withdrawStart(amount);
+    console.log(`Tx hash of withdraw tx is ${await withdrawTx.getTransactionHash()} and receipt is ${await withdrawTx.getReceipt()}`);
+    console.log(`New balance is ${await derc20Token.getBalance(address)}`);
+}
+
 (async function main() {
     const posClient = await getPosClient();    
     //await approve(posClient, '50');
-    await deposit(posClient, '50');
+    //await deposit(posClient, '50');
+    await withdraw(posClient, '100');
 })();
